af7ec79cccda34fb812fecbaa6d7e403bfdea13a
satanisticmicrowave <pavelchugunoff@gmail.com>
2022-07-05 11:47:20 +0300

    Initial commit

---
+++ src/main/java/com/chugunoff/algannah/AlgannahApplication.java
@@ -0,0 +1,13 @@
+package com.chugunoff.algannah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgannahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgannahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chugunoff/algannah/AlgannahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chugunoff.algannah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgannahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
